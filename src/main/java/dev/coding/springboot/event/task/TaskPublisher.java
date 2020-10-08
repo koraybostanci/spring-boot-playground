8a5e89072bfc167ebeed8a668534d57e49f357f9
Koray Bostancı <koraybostanci@users.noreply.github.com>
2020-10-08 19:43:39 +0200

    adds metrics collector for taskPublished and taskConsumed (#23)

--- src/main/java/dev/coding/springboot/event/task/TaskPublisher.java
+++ src/main/java/dev/coding/springboot/event/task/TaskPublisher.java
@@ -1,45 +1,52 @@
 package dev.coding.springboot.event.task;
 
+import dev.coding.springboot.configuration.MetricsCollector;
 import dev.coding.springboot.configuration.RabbitMQProperties;
 import dev.coding.springboot.event.EventPublisher;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import static java.lang.String.format;
+import static java.lang.System.currentTimeMillis;
 
 @Service
+@Slf4j
 public class TaskPublisher implements EventPublisher<Task> {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(TaskPublisher.class);
 
     private static final String PUBLISHING_TASK = "Publishing task to rabbitmq [{}]";
     private static final String SUCCESSFULLY_PUBLISHED_TASK = "Successfully published task to rabbitmq [{}]";
     private static final String FAILED_TO_PUBLISH_TASK = "Failed to publish task [%s] to rabbitmq due to [%s]";
 
+    private final MetricsCollector metricsCollector;
     private final RabbitTemplate rabbitTemplate;
     private final String exchangeName;
     private final String routingKey;
 
-    public TaskPublisher(final RabbitTemplate rabbitTemplate, final RabbitMQProperties rabbitMqProperties) {
+    public TaskPublisher(final RabbitTemplate rabbitTemplate, final RabbitMQProperties rabbitMqProperties, final MetricsCollector metricsCollector) {
         this.rabbitTemplate = rabbitTemplate;
         this.exchangeName = rabbitMqProperties.getExchangeName();
         this.routingKey = rabbitMqProperties.getTaskReceived().getRoutingKey();
+        this.metricsCollector = metricsCollector;
     }
 
     @Override
     public boolean publish(final Task task) {
-        LOGGER.info(PUBLISHING_TASK, task);
+        final long startedAt = currentTimeMillis();
+        log.info(PUBLISHING_TASK, task);
 
         try {
             rabbitTemplate.convertAndSend(exchangeName, routingKey, task);
-            LOGGER.info(SUCCESSFULLY_PUBLISHED_TASK, task);
+
+            log.info(SUCCESSFULLY_PUBLISHED_TASK, task);
+            metricsCollector.trackTaskPublished(currentTimeMillis() - startedAt);
+            
             return true;
         } catch (final AmqpException ex) {
-            LOGGER.error(format(FAILED_TO_PUBLISH_TASK, task, ex.getMessage()), ex);
+            log.error(format(FAILED_TO_PUBLISH_TASK, task, ex.getMessage()), ex);
         }
+
         return false;
     }
 }
